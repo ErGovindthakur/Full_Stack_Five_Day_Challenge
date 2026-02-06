27e3bda06fa7c9d58a080c8c15f0c7eb566852a4
ErGovindthakur <ergovindthakur@gmail.com>
2026-02-07 00:06:38 +0530

    starts revising my react concept

---
+++ 01_React_Revision/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HelloWorld from './components/HelloWorld'
+
+const App = () => {
+  return (
+    <div className='flex justify-center flex-wrap px-5 py-4'>
+    <HelloWorld />
+    </div>
+  )
+}
+
+export default App
---
+++ 01_React_Revision/src/components/HelloWorld.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HelloWorld = () => {
+  return (
+    <div className=' w-full px-2 py-1 bg-gray-700'>HelloWorld</div>
+  )
+}
+
+export default HelloWorld
